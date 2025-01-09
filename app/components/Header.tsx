9332bc98f15bd67ab239cc38405129b5d6448720
Abbalolo <verifiedaboki@gmail.com>
2025-01-09 19:55:10 +0100

    first commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
-import  { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function Header() {
   const [open, setOpen] = useState(false); // State to manage menu open/close
@@ -17,12 +16,10 @@ function Header() {
     }
   };
 
-  
   const handleToggleDarkMode = () => {
     const html = document.querySelector("html");
     if (!html) {
-return false
-     
+      return false;
     }
 
     if (theme === "light") {
@@ -32,17 +29,15 @@ return false
       html.classList.remove("dark");
       setTheme("light");
     }
-
   };
-
 
   useEffect(() => {
     // Attach scroll event listener
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Cleanup on component unmount
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -59,23 +54,39 @@ return false
       name: "About",
       link: "/#about",
     },
-  
   ];
 
+  useEffect(() => {
+    if (open) {
+      // Disable scrolling
+      document.body.style.overflow = "hidden";
+    } else {
+      // Enable scrolling
+      document.body.style.overflow = "";
+    }
+
+    // Cleanup on unmount
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [open]);
+
   return (
-    <header  className={`w-full flex  items-center justify-between px-10 py-3 fixed left-0 top-0 z-50  dark:text-[#ffffff] ${
-      isScrolled ? 'glass-effect text-[#ffffff]' : 'bg-transparent '
-    }`}>
+    <header
+      className={`w-full flex  items-center justify-between px-10 py-3 fixed left-0 top-0 z-50  dark:text-[#ffffff] ${
+        isScrolled ? "glass-effect text-[#ffffff]" : "bg-transparent "
+      }`}
+    >
       <div className="">
-      <Link className="text-[30px]  flex items-center" href="/">
-        JOSH EGESI
-      </Link>
+        <Link className="text-[30px]  flex items-center" href="/">
+          JOSH EGESI
+        </Link>
       </div>
 
       <nav
         className={` absolute w-full md:w-auto inter-font  top-0 left-0 md:static md:flex justify-center items-center md:justify-end md:items-end  transition-transform duration-500 ${
           open
-            ? "transform translate-x-0 h-screen flex flex-col  bg-[#0E1011]"
+            ? "transform translate-x-0 h-screen flex flex-col  bg-white dark:bg-[#0E1011]"
             : "transform -translate-x-full md:translate-x-0 md:h-auto "
         }`}
       >
@@ -83,7 +94,7 @@ return false
           {navData.map((data, index) => (
             <li key={index}>
               <Link
-              onClick={() => setOpen(false)}
+                onClick={() => setOpen(false)}
                 href={data.link}
                 className="relative before:content-[''] before:absolute before:top-6 before:left-0 before:w-0 before:h-[2px] before:bg-white before:transition-all before:duration-300 hover:before:w-full md:text-[20px] text-[40px]"
               >
@@ -91,20 +102,37 @@ return false
               </Link>
             </li>
           ))}
-        
         </ul>
-        <Link   onClick={() => setOpen(false)} href={"/contact"} className="border-b-2 hover:border-none transition-all duration-300  text-[20px] md:ml-10 font-semibold mt-5 md:mt-0">Lets Talk</Link>
-        <button   onClick={handleToggleDarkMode}
-        className="md:ml-5 h-10 w-10 rounded-lg p-2 hover:bg-gray-100 bg-white dark:bg-black dark:hover:bg-gray-700 transition-all duration-300">
-        <svg className="fill-black block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+        <Link
+          onClick={() => setOpen(false)}
+          href={"/contact"}
+          className="border-b-2 hover:border-none transition-all duration-300  text-[20px] md:ml-10 font-semibold mt-5 md:mt-0"
+        >
+          Lets Talk
+        </Link>
+        <button
+          onClick={handleToggleDarkMode}
+          className="md:ml-5 h-10 w-10 rounded-lg p-2 hover:bg-gray-100 bg-white dark:bg-black dark:hover:bg-gray-700 transition-all duration-300 absolute md:static top-5 left-5"
+        >
+          <svg
+            className="fill-black block dark:hidden"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-        </svg>
-        <svg className="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+          </svg>
+          <svg
+            className="fill-yellow-500 hidden dark:block"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
             <path
-                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                fill-rule="evenodd" clip-rule="evenodd"></path>
-        </svg>
-    </button>
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
       </nav>
 
       <div
@@ -133,8 +161,6 @@ return false
           ></span>
         </div>
       </div>
-      
-    
     </header>
   );
 }
